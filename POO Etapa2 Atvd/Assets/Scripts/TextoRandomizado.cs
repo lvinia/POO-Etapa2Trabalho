60981f4e1a27d85efd2e294b2af0d1f81593536a
lvinia <145567312+lvinia@users.noreply.github.com>
2025-07-02 16:22:35 -0300

    AulaD2M7

---
+++ POO Etapa2 Atvd/Assets/Scripts/TextoRandomizado.cs
@@ -0,0 +1,31 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TextoRandomizado : MonoBehaviour
+{
+    public string novoTexto;
+    public TextMeshProUGUI UITexto;
+    
+    public int numero;
+
+    private void Start()
+    {
+        UITexto.text = novoTexto + "  " + numero;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && numero > 0)
+        {
+            numero--;
+            UITexto.text = novoTexto + "  " + numero;
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && numero < 10)
+        {
+            numero++;
+            UITexto.text = novoTexto + "  " + numero;
+        }
+    }
+}
