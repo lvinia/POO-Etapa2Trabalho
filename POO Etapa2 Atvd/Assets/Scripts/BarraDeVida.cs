9f72459861267707f202c9c26fa678d797411f40
lvinia <145567312+lvinia@users.noreply.github.com>
2025-07-21 14:30:44 -0300

    AulaD21M7

---
+++ POO Etapa2 Atvd/Assets/Scripts/BarraDeVida.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BarraDeVida : MonoBehaviour
+{
+    
+    public Jogador jogador;
+    [SerializeField]
+    private int vidasRestantes = 0;
+    [SerializeField]
+    private int energiaRestante = 0;
+    
+    public Slider slider_vidasRestantes;
+    
+    public Slider slider_EnergiaRestante;    
+    void Start()
+    {
+        
+        // buscar jogador na cena:
+        if (jogador == null)
+        {
+            //jogador = GameObject.Find("Jogador").GetComponent<Jogador>();
+            jogador = GameObject.FindWithTag("Player").GetComponent<Jogador>();
+        }
+        
+        slider_vidasRestantes.minValue = 0;
+        slider_vidasRestantes.maxValue = jogador.getVidas();
+        
+        slider_EnergiaRestante.minValue = 0;
+        slider_EnergiaRestante.maxValue = jogador.getEnergia();
+
+        // vidasRestantes = jogador.getVidas();
+        // energiaRestante = jogador.getEnergia();
+    }
+
+    
+    void Update()
+    {
+        vidasRestantes = jogador.getVidas();
+        slider_vidasRestantes.value = vidasRestantes;
+        
+        energiaRestante = jogador.getEnergia();
+        slider_EnergiaRestante.value = energiaRestante;
+    }
+}
---
+++ POO Etapa2 Atvd/Assets/Scripts/Personagem.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    [SerializeField]
+    private int vidas;
+    [SerializeField]
+    private int energia;
+
+
+    public int getVidas()
+    {
+        return this.vidas;
+    }
+
+    public void setVidas(int vidas)
+    {
+        this.vidas = vidas;
+    }
+
+    public int getEnergia()
+    {
+        return this.energia;
+    }
+
+    public void setEnergia(int energia)
+    {
+        this.energia = energia;
+    }
+    
+}
